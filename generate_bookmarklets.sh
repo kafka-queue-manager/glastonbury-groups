0fa5cb06fef55dc8e7f3f8ae1c22dcd9d2d067a0
Leon Aves <leon.aves@lokalise.com>
2024-11-14 01:44:12 +0100

    Fix css paths.

--- generate_bookmarklets.sh
+++ generate_bookmarklets.sh
@@ -16,7 +16,7 @@ cat <<EOF
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/index.css">
+    <link rel="stylesheet" href="index.css">
     <title>Bookmarklets for Groups</title>
     <script>
         function updateBookmarklet(groupIndex) {
